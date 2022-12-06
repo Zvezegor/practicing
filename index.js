c3dea4c56c240bdcc964df8556aa3eaf66ece6d7
Zvezegor <ferocaritas@gmail.com>
2022-12-06 21:50:15 +0300

    added modal

--- index.js
+++ index.js
@@ -2,9 +2,43 @@ const prev = document.querySelector("#prev"),
   next = document.querySelector("#next"),
   slides = document.querySelectorAll(".item");
 
-slides.forEach((slide) => {
-  slide.style.display = "none";
-});
+function remuve() {
+  slides.forEach((slide) => {
+    slide.style.display = "none";
+  });
+}
+
 let n = 0;
+remuve();
 slides[n].classList.add("active");
-next.addEventListener("click", function () {});
+next.addEventListener("click", function () {
+  n = n + 1;
+  slides[n].classList.add("active");
+  slides[n - 1].classList.remove("active");
+});
+prev.addEventListener("click", function () {
+  n = n - 1;
+  if (n < 0) {
+    return (n = slides.length);
+  }
+  slides[n].classList.add("active");
+  slides[n + 1].classList.remove("active");
+});
+if (n > slides.length) {
+  n = 0;
+}
+if (n < 0) {
+  n = slides.length;
+}
+//modal
+
+const openModal = document.querySelector("#modal-open"),
+  modal = document.querySelector(".modal-wrapper"),
+  closeModal = document.querySelector("#modal-close");
+
+openModal.addEventListener("click", () => {
+  modal.classList.add("active");
+});
+closeModal.addEventListener("click", () => {
+  modal.classList.remove("active");
+});
